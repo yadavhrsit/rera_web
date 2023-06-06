fdc1b4c81c6a3348d04bc2c9e4de4704d9af741c
Harshit Yadav <78172595+yadavhrsit@users.noreply.github.com>
2023-06-06 14:23:36 +0530

    Update FeaturesCard.jsx

--- src/components/FeaturesCard.jsx
+++ src/components/FeaturesCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Card, CardContent, Typography } from '@mui/material';
 function FeaturesCard(props) {
     return (
-        <Card key={props.index} style={{ height: '100%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', color: '#212529', backgroundColor: '#dee2e6', padding: '30px 15px 0px 15px' }}>
+        <Card key={props.index} style={{ height: '80vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', color: '#212529', backgroundColor: '#dee2e6', padding: '30px 15px 20px 15px' }}>
             <CardContent style={{ display: 'flex', flexDirection: 'column', gap: '18px' }}>
                 <Typography variant="h4" sx={{ fontWeight: '600' }}>
                     {props.heading}
@@ -12,7 +12,7 @@ function FeaturesCard(props) {
                 </Typography>
             </CardContent>
             <div style={{ marginTop: 'auto' }}>
-                <img src={props.img} alt="Feature" style={{ width: '100%', height: '500px' }} />
+                <img src={props.img} alt="Feature" style={{ width: '100%', height: '100%' }} />
             </div>
         </Card>
     )
